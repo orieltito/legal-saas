f494373703aa2a6489354829ef65a1870bb7df64
orieltito <45402053+orieltito@users.noreply.github.com>
2026-04-04 04:03:45 +0000

    frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe1c97b9165ce8c0"; }
+/******/ 		__webpack_require__.h = function() { return "8801ff15317fdaf0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
