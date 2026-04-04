989ac2e3b94aa0a7a501ef0350decda23267de6e
orieltito <45402053+orieltito@users.noreply.github.com>
2026-04-04 04:17:08 +0000

    login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
 
     const data = await res.json();
 
-    localStorage.setItem('token', data.token);
+    localStorage.setItem("token", data.token);
     router.push('/dashboard');
   }
 
